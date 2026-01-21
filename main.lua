e7616a2132521874f9399605f63ae76411327815
Wyattluber <81474359+Wyattluber@users.noreply.github.com>
2026-01-21 20:14:31 +0100

    Update BASE_URL to point to Developer branch

--- main.lua
+++ main.lua
@@ -34,7 +34,7 @@ by Nebula Softworks
 
 
 
-local BASE_URL = "https://raw.githubusercontent.com/SorinSoftware-Services/AurexisInterfaceLibrary/main/"
+local BASE_URL = "https://raw.githubusercontent.com/SorinSoftware-Services/AurexisInterfaceLibrary/Developer/"
 
 local Release = "Pre Release [v 0.2.6]"
 
