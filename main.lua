722c79f8a3ba53abbddb8a0c586b285589bb8bda
Wyattluber <81474359+Wyattluber@users.noreply.github.com>
2026-02-06 17:41:46 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,7 +36,7 @@ by Nebula Softworks
 
 local BASE_URL = "https://raw.githubusercontent.com/SorinSoftware-Services/AurexisInterfaceLibrary/main/"
 
-local Release = "v 1.0.0"
+local Release = "v 1.0.1"
 
 local Aurexis = { 
 	Folder = "AurexisLibrary UI", 
