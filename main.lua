3bbf3f4b48bff0fa68a87ca4edbe7dce30ef3c83
EndOfCircuit <nemesisofnocturn@gmail.com>
2026-02-28 09:51:35 +0100

    Update release version to v 1.1.0

--- main.lua
+++ main.lua
@@ -36,7 +36,7 @@ by Nebula Softworks
 
 local BASE_URL = "https://raw.githubusercontent.com/SorinSoftware-Services/AurexisInterfaceLibrary/main/"
 
-local Release = "v 1.0.1"
+local Release = "v 1.1.0"
 
 local Aurexis = { 
 	Folder = "AurexisLibrary UI", 
