86f340d73724c6ccaa6063736e0fffee16a700b4
Wyattluber <81474359+Wyattluber@users.noreply.github.com>
2026-03-22 16:26:39 +0100

    Update release version to 1.1.1

--- main.lua
+++ main.lua
@@ -36,7 +36,7 @@ by Nebula Softworks
 
 local BASE_URL = "https://raw.githubusercontent.com/SorinSoftware-Services/AurexisInterfaceLibrary/main/"
 
-local Release = " Version 1.1.0"
+local Release = " Version 1.1.1"
 
 local Aurexis = { 
 	Folder = "AurexisLibrary UI", 
